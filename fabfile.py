c8a667c5407b0ca09214cc633e7666f13f7b096b
m <mmailm@mail.ru>
2015-01-09 19:12:11 +0800

    настроил fabfile для deploy

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,20 @@ from fabric.operations import local
 
 env.hosts = ['phosphorus.locum.ru']
 env.user = 'hosting_mmailm'
-env.activate = 'source /home/hosting_mmailm/projects/env/bin/activate'
+env.activate = 'source /home/hosting_mmailm/projects/env-irgid/bin/activate'
+
+app_dir = "/home/hosting_mmailm/projects/django-irgid"
 
 def deploy():
-    pass
+    local("ssh-add ~/.ssh/locum.ru")
+    local("git push --all -u")
+    with cd(app_dir):
+        with prefix(env.activate):
+            run("git stash")
+            run("git pull")
+            with settings(warn_only=True):
+                run("git stash apply")
+                run('pip-accel install -r requirements.txt')
+            run("python manage.py migrate")
+            run("python manage.py collectstatic --noinput")
+            run("touch django.wsgi")
