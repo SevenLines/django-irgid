e91e0e5b4f07174e341816baf9a3e271b128d590
m <mmailm@mail.ru>
2015-01-12 04:17:48 +0800

    поправил мелкий баг

--- app/assets.py
+++ app/assets.py
@@ -9,7 +9,6 @@ main_js = Bundle('bower/jquery/dist/jquery.min.js',
                  'bower/jquery.lazyload/jquery.lazyload.min.js',
                  'bower/jquery-impromptu/dist/jquery-impromptu.min.js',
                  'js/interface.js',
-                 'js/jquery.fixer.js',
                  filters="yui_js",
                  output="js/main.min.js")
 
