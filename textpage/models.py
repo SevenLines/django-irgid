87a49202b3400c54fd333efee0ee14ff6a795756
m <mmailm@mail.ru>
2015-01-15 18:46:08 +0800

    поправил ошибку из-за которой не удалялись файлы при удалении категории

--- textpage/models.py
+++ textpage/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 
 # Create your models here.
+from django.db.models.signals import post_delete
+from django.dispatch.dispatcher import receiver
 from easy_thumbnails.fields import ThumbnailerImageField
 from filer.fields.image import FilerImageField
 
@@ -14,3 +16,9 @@ class TextPage(CMSPlugin):
 
 class TextPageImage(models.Model):
     image = ThumbnailerImageField(upload_to="textpage")
+
+
+@receiver(post_delete, sender=TextPageImage)
+def mymodel_delete(sender, instance, **kwargs):
+    if instance.image:
+        instance.image.delete(False)
