0b221f3fa7cfff2a698f70eb28e27c04cb698286
m <mmailm@mail.ru>
2015-01-20 19:38:49 +0800

    чуток оптимизировал страницу

--- excursions/static/js/excursions/main.guest.js
+++ excursions/static/js/excursions/main.guest.js
@@ -14,7 +14,6 @@ window.ExcursionPriceComputer = function (price_list) {
         for (var i = 0; i < price_list.length; ++i) {
             item = price_list[i];
             if (item[0] <= ppl_count && ppl_count <= item[1]) {
-                console.log(item);
                 price = adults_count * item[3] + children_count * item[2] + item[4];
                 $("#priceOut").text(price);
                 if (item[4] && !item[3] && !item[2]) {
@@ -65,8 +64,23 @@ $(function () {
     }
 });
 
+// переносы
 $(function () {
     if (!$("html").is(".lt-ie9, .lt-ie8, .lt-ie7")) {
         $('#excursion-description, .description').hyphenate('ru');
     }
+});
+
+$(function () {
+    // кликанье на элементы галереи
+    //var $yashare = $("#yashare")first();
+    var galleryItems = $(".excursion-gallery-item");
+    galleryItems.on("click", function () {
+        $(".main-image").attr("src", $(this).data("thumbSrc"))
+            .parent().attr("href", $(this).data("src"));
+        galleryItems.removeClass("active");
+        $(this).addClass("active");
+        //$yashare.data("yashareImage", $(this).data("src"));
+    });
+
 });
