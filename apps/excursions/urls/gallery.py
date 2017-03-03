38ecff9066bedde445fc0d94bbb4dd0a5a768335
m <mmailm@mail.ru>
2017-03-03 11:55:11 +0300

    add verbose links

--- apps/excursions/urls/gallery.py
+++ apps/excursions/urls/gallery.py
@@ -7,6 +7,7 @@ from excursions.views import ExcursionGalleryItemView, ExcursionGalleryIndexView
 __author__ = 'm'
 
 urlpatterns = i18n_patterns('excursions.views',
+    url(r'(?P<pk>(\d+))/(?P<title>(.*?)$)', ExcursionGalleryItemView.as_view(), name='item'),
     url(r'(?P<pk>(\d+))/$', ExcursionGalleryItemView.as_view(), name='item'),
     url(r'$', ExcursionGalleryIndexView.as_view(), name='index'),
 )
